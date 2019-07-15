b540ac718d1cee690af0ef6b00a855b9b20c5fef
gjethwani <gdjethwa@usc.edu>
2019-07-16 02:48:39 +0800

    Remove debug statements

--- src/handlers/login.js
+++ src/handlers/login.js
@@ -1,14 +1,11 @@
 const { passport } = require('../passport')
 
 const logInHandler = function(req, res, next) {
-  console.log(process.env)
     passport.authenticate('local', { failureFlash: true}, function(err, user, info) {
       if (err) { 
         res.status(500).json({ err: JSON.stringify(err) }) 
       }
       if (!user) { 
-        console.log("here1")
-        console.log(info)
         return res.status(401).json({
           err: info
         }) 
