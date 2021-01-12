d15c4791b7b70a6b4c8ab32da389f95b4c91133d
gjethwani <gdjethwa@usc.edu>
2021-01-11 23:43:10 -0800

    Fix service all

--- src/handlers/service-all.js
+++ src/handlers/service-all.js
@@ -1,6 +1,14 @@
 const { addToQueue, log } = require('../util')
 const RequestModel = require('../models/request')
 
+const wait = (ms) => {
+    const start = Date.now()
+    let now = start
+    while (now - start < ms) {
+      now = Date.now()
+    }
+}
+
 const serviceAllHandler = (req, res) => {
     const { userId } = req.session
     let { accepted } = req.body
@@ -20,45 +28,47 @@ const serviceAllHandler = (req, res) => {
             return res.status(200).send()
         })
     } else {
-        RequestModel.find({ userId, serviced: false}, (err, requests) => {
+        let errFound = false
+        RequestModel.find({ userId, serviced: false}, async (err, requests) => {
             if (err) {
                 log('/service-all', userId, `[mongoose-find-err] ${JSON.stringify(err)}`)
                 return res.status(500).json({ err: JSON.stringify(err) })
             }
-            let finishedRequests = 0
-            requests.forEach(async r => {
-                addToQueue(r.songId, req.session.accessToken, async () => {
-                    try {
-                        await RequestModel.findByIdAndUpdate(r._id, { $set: { serviced: true, accepted }})
-                        return {
-                            status: 200
-                        }
-                    } catch(err) {
-                        return {
-                            status: 500,
-                            message: JSON.stringify(err)
-                        }
-                    }
-                })
-                    .then(response => {
-                        console.log("GAUTAM")
-                        console.log(response)
-                        if (response.status === 500) {
-                            log('/service-all', userId, `[mongoose-update-one-err] ${response.message}`)
-                            return res.status(500).json({ err: err.message })
-                        }
-                        // finishedRequests++
-                        // console.log(finishedRequests, requests.length)
-                        // if (finishedRequests === requests.length) {
-                        //     return res.status(200).send()
-                        // }
-                    })
-                    .catch(err => {
-                        log('/service-all', userId, `[add-to-queue-err] ${err.message}`)
-                        console.log("BREAK")
-                        return res.status(err.status).json({ err: err.message})
-                    })
+            requests.sort((r1, r2) => {
+                return new Date(r1.createdAt) - new Date(r2.createdAt)
             })
+            for (let i = 0; i < requests.length; i++) {
+                const r = requests[i]
+                try {
+                    const response = await addToQueue(r.songId, req.session.accessToken, async () => {
+                        try {
+                            await RequestModel.findByIdAndUpdate(r._id, { $set: { serviced: true, accepted }})
+                            return {
+                                status: 200
+                            }
+                        } catch(err) {
+                            return {
+                                status: 500,
+                                message: JSON.stringify(err)
+                            }
+                        }
+                    })
+                    if (response.status === 500) {
+                        log('/service-all', userId, `[spotify-err] ${response.status} ${response.message}`)
+                        errFound = true
+                        return res.status(500).json({ err: response.message })
+                    }
+                    wait(50)
+                } catch(err) {
+                    log('/service-all', userId, `[add-to-queue-err] ${err.message}`)
+                    errFound = true
+                    return res.status(err.status).json({ err: err.message })
+                }
+            }
+            console.log(errFound)
+            if (!errFound) {
+                return res.status(200).send()
+            }
         })
     }
 }
--- src/util.js
+++ src/util.js
@@ -16,7 +16,6 @@ const addToQueue = (songId, accessToken, successCallback) => {
         }
         requestModule.post(options, async (error, response, body) => {
             if (!error && response.statusCode >= 200 && response.statusCode < 300) {
-                console.log("hjere1")
                 resolve(successCallback())
             } else {
                 if (error) {
@@ -30,11 +29,13 @@ const addToQueue = (songId, accessToken, successCallback) => {
                             status: 404,
                             message: 'no queue'
                         })
+                    } else {
+                        reject({
+                            status: response.statusCode,
+                            message: `spotify error ${body}`
+                        })
                     }
-                    reject({
-                        status: response.statusCode,
-                        message: 'spotify error'
-                    })
+                    
                 }
             }
         })
